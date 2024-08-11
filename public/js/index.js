2e972d45bd9fa53917e4303fef4ae57bd8670193
Abubakar yasin <ayasiin@hotmail.com>
2024-08-11 15:30:07 +0100

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+// event listen
+btn.addEventListener('click',() =>{
+    btn.classList.toggle('open')
+})
